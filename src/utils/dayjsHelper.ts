80b1c62713c3b91b46f7c6be64806c2825f1904d
Meri <smt5783@gmail.com>
2026-01-12 19:26:45 +0800

    feat: dayjsHelper 新增 getFormatTime

--- src/utils/dayjsHelper.ts
+++ src/utils/dayjsHelper.ts
@@ -1,4 +1,4 @@
-import dayjs from 'dayjs';
+import dayjs from "dayjs";
 
 /**
  * 用於指定日期格式化的類型
@@ -6,7 +6,7 @@ import dayjs from 'dayjs';
  * - 'endOfDay'：回傳當天結束時間的 timestamp
  * - 'valueOf'：回傳該時間的 timestamp（預設）
  */
-export type getFormatDayType = 'startOfDay' | 'endOfDay' | 'valueOf';
+export type getFormatDayType = "startOfDay" | "endOfDay" | "valueOf";
 
 /**
  * 取得格式化後的日期 timestamp
@@ -16,18 +16,26 @@ export type getFormatDayType = 'startOfDay' | 'endOfDay' | 'valueOf';
  */
 export const getFormatDay = (
   date: string | number | Date | dayjs.Dayjs | null | undefined,
-  type: getFormatDayType = 'valueOf',
+  type: getFormatDayType = "valueOf"
 ) => {
   // 如果 type 是 'startOfDay'，回傳該日期的起始時間 timestamp
-  if (type === 'startOfDay') {
-    return dayjs(date).startOf('day').valueOf();
+  if (type === "startOfDay") {
+    return dayjs(date).startOf("day").valueOf();
   }
 
   // 如果 type 是 'endOfDay'，回傳該日期的結束時間 timestamp
-  if (type === 'endOfDay') {
-    return dayjs(date).endOf('day').valueOf();
+  if (type === "endOfDay") {
+    return dayjs(date).endOf("day").valueOf();
   }
 
   // 預設回傳傳入日期的 timestamp
   return dayjs(date).valueOf();
+};
+
+// 取得格式化後的時間字串
+export const getFormatTime = (
+  date: string | number | Date | dayjs.Dayjs | null | undefined,
+  formatString: string
+) => {
+  return dayjs(date).format(formatString);
 };
