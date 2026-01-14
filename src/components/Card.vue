bd5707368b47d209032901b78128986f084b4d8e
Meri <smt5783@gmail.com>
2026-01-14 18:17:33 +0800

    feat: Card 元件 UI 優化

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Button from "@/components/Button.vue";
+import { Calendar } from "@element-plus/icons-vue";
 
 const props = withDefaults(
   defineProps<{
@@ -21,29 +22,45 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <el-card class="border border-gray-300 rounded-md group">
+  <el-card class="card-border group bg-white-light">
     <!-- 圖片 -->
-    <figure class="overflow-hidden">
+    <figure class="overflow-hidden rounded-md">
       <img
         :src="imageUrl"
         :alt="`${title} 圖片`"
-        class="object-cover rounded-md transition-transform duration-300 group-hover:scale-110"
+        class="object-cover transition-transform duration-300 group-hover:scale-110"
       />
     </figure>
-    <!-- 標題 -->
-    <h3 class="text-lg font-bold mb-2">{{ title }}</h3>
-    <!-- 時間 -->
-    <p class="text-sm text-gray-500">{{ dateTime }}</p>
-    <!-- 按鈕 -->
-    <div class="flex justify-end">
-      <Button
-        type="primary"
-        size="small"
-        class="rounded-full text-sm"
-        @click="emit('click')"
-      >
-        {{ buttonText }}
-      </Button>
+    <!-- 內容區域 -->
+    <div class="mt-4">
+      <!-- 標題 -->
+      <h3 class="text-2xl font-bold text-secondary-dark">{{ title }}</h3>
+      <!-- 時間 -->
+      <p class="text-sm text-secondary mt-2 flex items-center gap-2">
+        <Calendar class="w-[20px] text-primary" />
+        {{ dateTime }}
+      </p>
+      <!-- 按鈕 -->
+      <div class="flex justify-end">
+        <Button
+          class="rounded-full text-base bg-info text-white"
+          @click="emit('click')"
+        >
+          {{ buttonText }}
+        </Button>
+      </div>
     </div>
   </el-card>
 </template>
+
+<style scoped>
+.card-border {
+  border-radius: 16px;
+  /* 透明化 border，改用 background-image 作為漸層邊框*/
+  border: 4px solid transparent;
+  background-image: linear-gradient(white, white),
+    linear-gradient(135deg, #ffe7c7, #c89850);
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+}
+</style>
