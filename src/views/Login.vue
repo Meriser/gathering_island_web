611acc0685d2f8b91f6dfb67a79fbccbb1bb1c7c
Meri <smt5783@gmail.com>
2026-02-13 16:54:30 +0800

    feat: 更新 Login 登入頁

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router";
 import { useAuthStore } from "@/stores/auth";
-import { User, Lock } from "@element-plus/icons-vue";
 import { useForm } from "vee-validate";
 import { toTypedSchema } from "@vee-validate/zod";
 import { loginSchema, type LoginFormData } from "@/schemas/auth";
-import VeeInput from "@/components/vee/VeeInput.vue";
+import VeeInputLabel from "@/components/vee/VeeInputLabel.vue";
 import useAlert from "@/hooks/useAlert";
 import type { LoginResponse } from "@/types/auth";
 import Button from "@/components/Button.vue";
 import { useLogin } from "@/api/auth/hook";
+import { Mail, Lock } from 'lucide-vue-next';
 
 // 路由
 const router = useRouter();
@@ -53,7 +53,7 @@ const { handleSubmit } = useForm<LoginFormData>({
 });
 
 // 使用 TanStack Query Hook 登入
-const { mutate, isPending, error } = useLogin(successLogin);
+const { mutate, isPending } = useLogin(successLogin);
 
 // 處理登入表單提交
 const handleLogin = handleSubmit((values) => {
@@ -63,86 +63,38 @@ const handleLogin = handleSubmit((values) => {
 
 <template>
   <!-- 外層：背景圖片容器 -->
-  <div
-    class="min-h-screen bg-island_bg bg-cover bg-center relative"
-  >
+  <div class="min-h-screen bg-island_bg bg-cover bg-center relative">
     <!-- 漸層覆蓋層：半透明 -->
     <div class="absolute inset-0 bg-black bg-opacity-50"></div>
 
     <!-- 內容層 -->
-    <div
-      class="min-h-screen flex items-center justify-center p-4 relative z-10"
-    >
-      <div class="w-full max-w-[440px]">
-        <el-card shadow="always" class="rounded-xl">
-          <template #header>
-            <figure>
-              <!-- 回首頁圖片 -->
-              <img
-                @click="handleGoHome"
-                class="cursor-pointer w-[220px] mx-auto"
-                src="/logo_title.svg"
-                alt="Logo"
-              />
-            </figure>
-          </template>
+    <div class="min-h-screen flex items-center justify-center p-4 relative z-10">
+      <div class="w-full max-w-[440px] bg-white rounded-xl px-4 py-8">
+        <figure>
+          <!-- 回首頁圖片 -->
+          <img @click="handleGoHome" class="cursor-pointer w-[220px] mx-auto" src="/logo_title.svg" alt="Logo" />
+        </figure>
 
-          <el-form @submit.prevent="handleLogin" class="py-4 px-8">
-            <VeeInput
-              name="email"
-              label="信箱"
-              placeholder="請輸入信箱"
-              type="email"
-              size="large"
-              inputClass="w-full"
-              :prefix-icon="User"
-              required
-            />
+        <el-form @submit.prevent="handleLogin" class="py-4 px-8 mt-4">
+          <!-- 信箱 -->
+          <VeeInputLabel name="email" label="信箱" placeholder="請輸入信箱" type="email" :prefix-icon="Mail" />
+          <!-- 密碼 -->
+          <VeeInputLabel name="password" label="密碼" placeholder="請輸入密碼" type="password" :show-password="true"
+            :prefix-icon="Lock" />
 
-            <VeeInput
-              name="password"
-              label="密碼"
-              placeholder="請輸入密碼"
-              type="password"
-              size="large"
-              inputClass="w-full"
-              :prefix-icon="Lock"
-              required
-            />
-
-            <!-- 錯誤提示 -->
-            <el-alert
-              v-if="error?.message"
-              :title="error?.message"
-              type="error"
-              :closable="true"
-              show-icon
-            />
-
-            <!-- 操作按鈕區域 -->
-            <div class="flex gap-4 mt-8">
-              <!-- 探索按鈕 -->
-              <Button
-                type="primary"
-                size="large"
-                class="w-full rounded-full text-xl"
-                plain
-              >
-                探索
-              </Button>
-              <!-- 登入按鈕 -->
-              <Button
-                type="primary"
-                size="large"
-                native-type="submit"
-                :loading="isPending"
-                class="w-full rounded-full text-xl"
-              >
-                {{ isPending ? "登入中..." : "登入" }}
-              </Button>
-            </div>
-          </el-form>
-        </el-card>
+          <!-- 操作按鈕區域 -->
+          <div class="flex gap-4 mt-8">
+            <!-- 探索按鈕 -->
+            <Button class="w-full rounded-full text-xl" variant="outline">
+              前往註冊
+            </Button>
+            <!-- 登入按鈕 -->
+            <Button native-type="submit" :loading="isPending" class="w-full rounded-full text-xl">
+              <span v-if="!isPending">登入</span>
+              <span v-else>登入中</span>
+            </Button>
+          </div>
+        </el-form>
       </div>
     </div>
   </div>
